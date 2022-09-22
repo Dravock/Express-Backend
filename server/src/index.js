309111b18db2ba86b7fb18fd40cd42d02a95bbdd
Deryan Keskin <Deryan@keskin-neubulach.de>
2022-09-22 17:28:41 +0200

    simpler server

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(3001, ()=>{
+    var currentdate = new Date(); 
+    var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    console.log(datetime,': server is running on 3001')
+})
