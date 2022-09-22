554db83399d7d558041a1e79634554c10b714f2a
Deryan Keskin <Deryan@keskin-neubulach.de>
2022-09-22 17:40:33 +0200

    Fertiges Server Template

--- server/src/index.js
+++ server/src/index.js
@@ -6,13 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+
 app.listen(3001, ()=>{
     var currentdate = new Date(); 
-    var datetime = "Last Sync: " + currentdate.getDate() + "/"
-                + (currentdate.getMonth()+1)  + "/" 
-                + currentdate.getFullYear() + " @ "  
-                + currentdate.getHours() + ":"  
-                + currentdate.getMinutes() + ":" 
-                + currentdate.getSeconds();
-    console.log(datetime,': server is running on 3001')
+    var datetime = "Last Sync: "
+                                + currentdate.getHours()        + ":"  
+                                + currentdate.getMinutes()      + ":" 
+                                + currentdate.getSeconds();
+    console.log(datetime,'=> server is running on 3001')
 })
